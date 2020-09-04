a9b7460a26fbe12dba547c05b5d4c839f5ae9cb4
jonathan warner <j.m.warners@gmail.com>
2020-09-04 10:02:13 -0700

    MVP building complete. Testing now

--- data/helpers/action-router.js
+++ data/helpers/action-router.js
@@ -50,55 +50,41 @@ router.post('/', (req, res) => {
         });
 });
 
-// DELETE - Remove a project by id
+// DELETE - Remove an action  by id
 router.delete('/:id', (req, res) => {
-    Projects.remove(req.params.id)
+    Actions.remove(req.params.id)
     .then(count => {
         if (count > 0) {
-            res.status(200).json({ message: 'The project has been deleted' });
+            res.status(200).json({ message: 'The action has been deleted' });
         } else {
-            res.status(404).json({ message: 'The project could not be found' });
+            res.status(404).json({ message: 'The action could not be found' });
         }
     })
     .catch(error => {
         console.log(error);
         res.status(500).json({
-          message: 'Error removing the project',
+          message: 'Error removing the action',
         });
       });
 });
 
-// PUT - Update the project by id
+// PUT - Update the action by id
 router.put('/:id', (req, res) => {
-    Projects.update(req.params.id, req.body)
-    .then(project => {
-        if (project) {
-            res.status(200).json(project);
+    Actions.update(req.params.id, req.body)
+    .then(action => {
+        if (action) {
+            res.status(200).json(action);
         } else {
-            res.status(404).json({ message: 'The project could not be found' });
+            res.status(404).json({ message: 'The action could not be found' });
         }
     })
     .catch(error => {
         console.log(error);
         res.status(500).json({
-          message: 'Error updating the project',
+          message: 'Error updating the action',
         });
       });
 });
-
-// GET - Get all actions for a project by id
-router.get('/:id/actions', (req, res) => {
-    Projects.getProjectActions(req.params.id)
-    .then(actions => {
-        res.status(200).json(actions);
-    })
-    .catch (error => {
-        console.log(error);
-        res.status(500).json({
-          message: 'Error getting the actions for the project',
-        });
-      });
-}); 
 
 module.exports = router; 
 
